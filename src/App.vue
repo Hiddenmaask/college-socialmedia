c3db7e339adeb2ef54c48d11b0adb1f335d09613
Theo Crosby <iihiddenmaskii@gmail.com>
2022-10-25 16:30:42 +0100

    [Addition] Resizing

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,405 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="nav-bar">
+    <div class="nav-container">
+      <ul class="nav-content">
+        <li class="">
+            <svg id="logo-svg" width="195" height="61" viewBox="0 0 195 61" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M27.6067 18.2707L25.1957 21.3649L38.4565 26.5219V25.4905V23.4277L37.251 21.3649L36.0455 19.3021L32.4289 17.2393L27.6067 18.2707Z" fill="#0ACF84"/>
+              <path d="M24.4683 60.9964L28.1619 56.421C29.6227 56.3971 31.2023 56.2541 32.4968 55.6345C37.1168 53.4183 37.0574 48.259 37.0455 48.0326C37.0336 47.4368 36.9505 47.5083 33.6132 43.5762H43.0075C42.9837 49.5458 42.1405 51.7382 41.7961 52.5961C40.6797 55.4201 36.5823 60.079 30.4897 60.8654C29.4564 60.9964 26.5942 61.0084 24.4683 60.9964ZM48.2688 41.0144C52.8531 41.0144 55.2165 39.394 57.0217 37.3326L36.1785 37.2969L34.112 41.0264L48.2688 41.0144ZM43.0906 14.336V34.3895H34.3614L30.4778 41.0025H29.7058C23.4944 41.0025 22.8056 34.2704 22.7818 33.9844C22.6393 32.4116 20.2521 32.6022 20.3947 34.1989C20.5609 36.0815 21.4398 38.965 23.5301 41.0025H12.7225C9.69395 41.0025 -0.436694 37.8092 0.014612 27.4191V2.00366C0.014612 0.0972109 1.16663 0.025719 1.79608 0.00188831H11.5348C11.5586 0.00188831 11.5704 0.0138036 11.5942 0.0138036C12.1049 -0.0338577 17.5443 -0.236419 17.5443 5.2208C17.5443 6.80554 19.9434 6.81745 19.9434 5.2208C19.9434 3.05221 19.2901 1.31257 18.0669 0.0138036H28.0669C40.9766 -0.0338577 43.0906 9.47457 43.0906 14.336ZM27.3543 21.0086L34.9552 24.4998C36.5348 19.0783 29.9434 15.9565 27.3543 21.0086Z" fill="#1B1B1B"/>
+              <path d="M65.4 36.42C64.18 36.42 63.05 36.12 62.01 35.52C60.97 34.92 60.13 34.06 59.49 32.94C58.87 31.8 58.56 30.43 58.56 28.83V28.38C58.56 26.8 58.87 25.44 59.49 24.3C60.11 23.16 60.94 22.29 61.98 21.69C63.02 21.09 64.16 20.79 65.4 20.79C66.36 20.79 67.16 20.91 67.8 21.15C68.46 21.39 69 21.7 69.42 22.08C69.84 22.44 70.16 22.82 70.38 23.22H70.86V15H73.95V36H70.92V33.9H70.44C70.06 34.54 69.48 35.12 68.7 35.64C67.94 36.16 66.84 36.42 65.4 36.42ZM66.27 33.72C67.61 33.72 68.71 33.29 69.57 32.43C70.45 31.57 70.89 30.34 70.89 28.74V28.47C70.89 26.89 70.46 25.67 69.6 24.81C68.74 23.95 67.63 23.52 66.27 23.52C64.95 23.52 63.85 23.95 62.97 24.81C62.11 25.67 61.68 26.89 61.68 28.47V28.74C61.68 30.34 62.11 31.57 62.97 32.43C63.85 33.29 64.95 33.72 66.27 33.72ZM85.2788 36.42C83.7788 36.42 82.4688 36.11 81.3488 35.49C80.2288 34.85 79.3488 33.96 78.7088 32.82C78.0888 31.66 77.7788 30.32 77.7788 28.8V28.44C77.7788 26.9 78.0888 25.56 78.7088 24.42C79.3288 23.26 80.1888 22.37 81.2888 21.75C82.4088 21.11 83.6988 20.79 85.1588 20.79C86.5788 20.79 87.8188 21.11 88.8788 21.75C89.9588 22.37 90.7988 23.24 91.3988 24.36C91.9988 25.48 92.2988 26.79 92.2988 28.29V29.46H80.9288C80.9688 30.76 81.3988 31.8 82.2188 32.58C83.0588 33.34 84.0988 33.72 85.3388 33.72C86.4988 33.72 87.3688 33.46 87.9488 32.94C88.5488 32.42 89.0088 31.82 89.3288 31.14L91.8788 32.46C91.5988 33.02 91.1888 33.61 90.6488 34.23C90.1288 34.85 89.4388 35.37 88.5788 35.79C87.7188 36.21 86.6188 36.42 85.2788 36.42ZM80.9588 27.09H89.1488C89.0688 25.97 88.6688 25.1 87.9488 24.48C87.2288 23.84 86.2888 23.52 85.1288 23.52C83.9688 23.52 83.0188 23.84 82.2788 24.48C81.5588 25.1 81.1188 25.97 80.9588 27.09ZM98.9109 36L93.9309 21.21H97.2009L101.131 33.72H101.611L105.541 21.21H108.811L103.831 36H98.9109ZM128.726 36C127.826 36 127.106 35.73 126.566 35.19C126.046 34.65 125.786 33.93 125.786 33.03V23.82H121.706V21.21H125.786V16.32H128.876V21.21H133.286V23.82H128.876V32.49C128.876 33.09 129.156 33.39 129.716 33.39H132.806V36H128.726ZM136.917 36V21.21H139.947V22.95H140.427C140.667 22.33 141.047 21.88 141.567 21.6C142.107 21.3 142.767 21.15 143.547 21.15H145.317V23.94H143.427C142.427 23.94 141.607 24.22 140.967 24.78C140.327 25.32 140.007 26.16 140.007 27.3V36H136.917ZM148.518 36V21.21H151.608V36H148.518ZM150.078 19.2C149.478 19.2 148.968 19.01 148.548 18.63C148.148 18.23 147.948 17.72 147.948 17.1C147.948 16.48 148.148 15.98 148.548 15.6C148.968 15.2 149.478 15 150.078 15C150.698 15 151.208 15.2 151.608 15.6C152.008 15.98 152.208 16.48 152.208 17.1C152.208 17.72 152.008 18.23 151.608 18.63C151.208 19.01 150.698 19.2 150.078 19.2ZM164.744 36.42C163.304 36.42 162.194 36.16 161.414 35.64C160.654 35.12 160.084 34.54 159.704 33.9H159.224V36H156.194V15H159.284V23.22H159.764C160.004 22.82 160.324 22.44 160.724 22.08C161.124 21.7 161.654 21.39 162.314 21.15C162.974 20.91 163.784 20.79 164.744 20.79C165.984 20.79 167.124 21.09 168.164 21.69C169.204 22.29 170.034 23.16 170.654 24.3C171.274 25.44 171.584 26.8 171.584 28.38V28.83C171.584 30.43 171.264 31.8 170.624 32.94C170.004 34.06 169.174 34.92 168.134 35.52C167.114 36.12 165.984 36.42 164.744 36.42ZM163.844 33.72C165.184 33.72 166.284 33.29 167.144 32.43C168.024 31.57 168.464 30.34 168.464 28.74V28.47C168.464 26.89 168.034 25.67 167.174 24.81C166.314 23.95 165.204 23.52 163.844 23.52C162.524 23.52 161.424 23.95 160.544 24.81C159.684 25.67 159.254 26.89 159.254 28.47V28.74C159.254 30.34 159.684 31.57 160.544 32.43C161.424 33.29 162.524 33.72 163.844 33.72ZM182.193 36.42C180.693 36.42 179.383 36.11 178.263 35.49C177.143 34.85 176.263 33.96 175.623 32.82C175.003 31.66 174.693 30.32 174.693 28.8V28.44C174.693 26.9 175.003 25.56 175.623 24.42C176.243 23.26 177.103 22.37 178.203 21.75C179.323 21.11 180.613 20.79 182.073 20.79C183.493 20.79 184.733 21.11 185.793 21.75C186.873 22.37 187.713 23.24 188.313 24.36C188.913 25.48 189.213 26.79 189.213 28.29V29.46H177.843C177.883 30.76 178.313 31.8 179.133 32.58C179.973 33.34 181.013 33.72 182.253 33.72C183.413 33.72 184.283 33.46 184.863 32.94C185.463 32.42 185.923 31.82 186.243 31.14L188.793 32.46C188.513 33.02 188.103 33.61 187.563 34.23C187.043 34.85 186.353 35.37 185.493 35.79C184.633 36.21 183.533 36.42 182.193 36.42ZM177.873 27.09H186.063C185.983 25.97 185.583 25.1 184.863 24.48C184.143 23.84 183.203 23.52 182.043 23.52C180.883 23.52 179.933 23.84 179.193 24.48C178.473 25.1 178.033 25.97 177.873 27.09Z" fill="#1B1B1B"/>
+              <path d="M109.289 42L118.109 15H121.199L112.379 42H109.289Z" fill="#0ACF84"/>
+            </svg>
+        </li>
+        <li>
+          <a href="#">
+            Explore
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Features
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Our Mission
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Support
+          </a>
+        </li>
+        <ul class="nav-cta-wrapper">
+          <button type="button" class="input-btn-header"><h3>Login</h3></button>
+          <button type="button" class="input-btn-header"><h3>Register</h3></button>
+
+        </ul>
+      </ul>
+    </div>
+
+  </div>
+  <div id="outer-wrapper">
+    <header class="h-header">
+      <div class="h-header-container">
+        <div class="h-header-wrapper">
+          <h1 style="margin-bottom: 1ch;">
+            The largest developer network <p class="underline">whenever</p> you want it.
+          </h1>
+          <h2 style="max-width: 100%; font-weight: 400">
+            Join <a class="txt-highlight">365,000</a> other users and increase your workflow.
+          </h2>
+        </div>
+        <div class="h-wrapper">
+          <RegisterForm></RegisterForm>
+        </div>
+      </div>
+    </header>
+    <footer>
+      <div class="footer-container">
+        <div class="footer-content">
+          Privacy
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RegisterForm from './components/RegisterForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RegisterForm
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+
+*, *::after, *::before {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+
 }
+
+article, aside, details, figcaption, figure, footer, header, hgroup, main, nav, section, summary {
+  display: block;
+}
+
+
+
+.container, section{
+  overflow: visible;
+}
+
+button {
+  font: inherit;
+  border:none;
+  cursor: pointer;
+}
+
+input,
+label {
+  font: inherit;
+}
+
+html, #app, body{
+
+}
+
+
+:root{
+  --_hue: 346;
+  --_size: clamp(2rem, -4vw + 6rem, 4.5rem);
+  --_radius: 4px;
+  --_tspeed_fast: 150ms;
+  --_tspeed_slow: 400ms;
+  --_ttype_squish: cubic-bezier(0.86,-0.1, 0.27, 1.15);
+
+  /* Light Theme */
+
+  --bg--light: 266 100% 68%;
+  --con--light: 0 0% 100%;
+  --txt--light:  0 0% 11%;
+  --accent--light: 146 100% 66%;
+  --accent-secondary--light: 26 100% 68%;
+  --accent-tertiary--light: 266 100% 68%;
+
+  /* Dark Theme */
+
+  --bg--dark: var(--_hue) 100% 42%;
+  --con--dark: var(--_hue) 15% 99%;
+  --txt--dark: 0 0% 20%;
+  --accent--dark: 146 100% 66%;
+  --accent-secondary--dark: 26 100% 68%;
+  --accent-tertiary--dark: 266 100% 68%;
+
+  /* Defaults */
+
+  --bg: var(--bg--light);
+  --con: var(--con--light);
+  --txt: var(--txt--light);
+  --accent: var(--accent--light);
+  --accent-secondary: var(--accent-secondary--light);
+  --accent-tertiary: var(--accent-tertiary--light);
+  color-scheme: light;
+
+
+}
+
+/* @Media controls */
+
+@media (prefers-color-scheme: light){
+
+}
+
+@media (prefers-color-scheme: dark){
+  :root{
+    --bg: var(--bg--dark);
+    --con: var(--con--dark);
+    --txt: var(--txt--dark);
+    --accent: var(--accent--dark);
+    --accent-secondary: var(--accent-secondary--dark);
+    --accent-tertiary: var(--accent-tertiary--dark);
+    color-scheme: dark;
+  }
+
+
+}
+
+
+/* Prefers reduced motion */
+
+@media (prefers-reduced-motion: reduce) {
+  :root{
+    --_tspeed_fast: 50ms;
+    --_tspeed_slow: 100ms;
+
+  }
+}
+
+#app{
+  width:100%;
+  height: 100%;
+}
+body{
+  /*box-shadow: inset 0 0 calc(var(--_size)*1) calc(var(--_size)/ 1.5) hsl(var(--txt)/ .1);*/
+  color: hsl(var(--txt)) !important;
+  display: grid;
+  min-height: 100vh;
+  max-width: 100vw;
+  place-items: center;
+
+
+}
+
+
+footer{
+  background: hsl(var(--con));
+  clear: both;
+  color: hsl(var(--txt));
+  margin: 0;
+  padding-top: 24px;
+  width: 100%;
+  height: 100%;
+}
+
+html {
+  /* 62.5% of 16px browser font size is 10px */
+  font-size: 62.5%;
+  font-family: 'JetBrains Mono', monospace;
+}
+
+#outer-wrapper > *{
+  padding: calc(var(--_size)*1.4);
+  padding-inline: calc(var(--_size)*2);
+}
+
+
+
+
+.h-header{
+  background: hsl(var(--bg)) top/cover no-repeat url("@/assets/grid-hero.svg") fixed;
+
+}
+
+.h-header-container{
+  max-width: 1440px;
+  margin: 0 auto;
+}
+
+.h-header-underline{
+  background: hsl(var(--accent-secondary));
+  width: 20%;
+  height: 10px;
+}
+
+.h-header-wrapper *{
+  color: #FFFFFF;
+
+}
+
+.h-header-wrapper > *{
+  max-width: 80%;
+}
+
+.h-header-wrapper{
+  zoom: 1;
+  max-width: 1240px;
+  min-width: 256px;
+  padding: 0 24px;
+  position: relative;
+}
+
+svg * {
+  width:  100%;
+  height: 100%;
+}
+
+
+::selection {
+  background: hsl(var(--accent) / 0.8);
+  color: hsl(var(--bg));
+}
+
+.nav-bar{
+  background: hsl(var(--con));
+  border: 5px solid hsl(var(--txt));
+}
+
+.nav-content{
+  display: grid;
+  grid-template-columns: 2fr 1fr 1fr 1fr 1fr 2fr;
+  grid-auto-flow: column;
+  list-style: none;
+  justify-content: space-around;
+  align-items: center;
+  justify-items: center;
+}
+
+.nav-cta-wrapper{
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: 1fr 1fr;
+}
+
+ul > li {
+  white-space: nowrap;
+  padding: calc(var(--_size));
+  font-size: calc(var(--_size)*1);
+
+}
+
+.h-wrapper{
+  zoom: 1;
+  margin: 0 auto;
+  max-width: 1000px;
+  min-width: 256px;
+  padding: 48px 24px;
+  position: relative;
+}
+
+h1{
+  font-size: calc(var(--_size)*1.8);
+}
+
+h2{
+  font-size: calc(var(--_size)*1.4);
+}
+
+h3{
+  font-size: calc(var(--_size)*1);
+}
+
+h4{
+  font-size: calc(var(--_size)*.8);
+}
+
+
+.underline{
+  position : relative;
+  display : inline-block;
+  z-index: 0;
+}
+
+.underline::after {
+  content : "";
+  position : absolute;
+  width : 100%;
+  height : 0.5ch;
+  background-color: hsl(var(--accent-secondary));
+  bottom: -.5ch;
+  left: 0;
+  isolation: isolate;
+}
+
+.box-shadow{
+  box-shadow: -20px 20px 0px -5px hsl(var(--txt));
+}
+
+.input-btn{
+  box-shadow: -10px 10px 0px -5px hsl(var(--txt));
+  background: hsl(var(--accent-tertiary));
+  padding: calc(var(--_size));
+}
+
+.input-btn-header{
+  background: #FFFFFF;
+  width: 100%;
+  border-left: 5px solid hsl(var(--txt));
+}
+
+.input-btn-header:nth-child(2){
+  background: hsl(var(--accent));
+}
+
+.input-txt{
+  border: none;
+  border-bottom: 5px solid hsl(var(--txt));
+  outline: none;
+}
+
+.input-txt::placeholder{
+  color: hsl(var(--txt));
+}
+
+a{
+  margin: 0 auto;
+  color: hsl(var(--txt));
+  font-weight: 500;
+}
+
+.a-cta{
+  color: hsl(var(--accent-tertiary));
+  text-decoration: underline ;
+}
+
+.input-group{
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  justify-content: center;
+
+}
+
+.txt-highlight{
+  color: hsl(var(--accent-secondary));
+  font-weight: 600;
+}
+
+li:has(> #logo-svg){
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  border-right: 5px solid hsl(var(--txt));
+  padding-bottom: 0;
+}
+
 </style>
---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="register-container box-shadow">
+    <div class="register-wrapper ">
+      <div class="register-header">
+        <h3>Start your journey here.</h3>
+
+      </div>
+      <div class="register-content">
+        <form class="register-input">
+          <h3 class="underline">Hello there.</h3>
+          <input type="text" class="input-txt" placeholder="EMAIL">
+          <input type="password" class="input-txt" placeholder="PASSWORD">
+          <input type="password" class="input-txt" placeholder="CONFIRM PASSWORD">
+          <div class="input-group">
+            <button type="submit" class="input-btn"><h3 style="width: auto;">Register</h3></button>
+            <a class="a-cta" href="#"><h4>Terms and Conditions</h4></a>
+          </div>
+
+        </form>
+        <div class="register-logo">
+          <svg viewBox="0 0 61 61" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M27.6067 18.2707L25.1957 21.3649L38.4565 26.5219V25.4905V23.4277L37.251 21.3649L36.0455 19.3021L32.4289 17.2393L27.6067 18.2707Z" fill="#0ACF84"/>
+            <path d="M24.4683 60.9964L28.1619 56.421C29.6227 56.3971 31.2023 56.2541 32.4968 55.6345C37.1168 53.4183 37.0574 48.259 37.0455 48.0326C37.0336 47.4368 36.9505 47.5083 33.6132 43.5762H43.0075C42.9837 49.5458 42.1405 51.7382 41.7961 52.5961C40.6797 55.4201 36.5823 60.079 30.4897 60.8654C29.4564 60.9964 26.5942 61.0084 24.4683 60.9964ZM48.2688 41.0144C52.8531 41.0144 55.2165 39.394 57.0217 37.3326L36.1785 37.2969L34.112 41.0264L48.2688 41.0144ZM43.0906 14.336V34.3895H34.3614L30.4778 41.0025H29.7058C23.4944 41.0025 22.8056 34.2704 22.7818 33.9844C22.6393 32.4116 20.2521 32.6022 20.3947 34.1989C20.5609 36.0815 21.4398 38.965 23.5301 41.0025H12.7225C9.69395 41.0025 -0.436694 37.8092 0.014612 27.4191V2.00366C0.014612 0.0972109 1.16663 0.025719 1.79608 0.00188831H11.5348C11.5586 0.00188831 11.5704 0.0138036 11.5942 0.0138036C12.1049 -0.0338577 17.5443 -0.236419 17.5443 5.2208C17.5443 6.80554 19.9434 6.81745 19.9434 5.2208C19.9434 3.05221 19.2901 1.31257 18.0669 0.0138036H28.0669C40.9766 -0.0338577 43.0906 9.47457 43.0906 14.336ZM27.3543 21.0086L34.9552 24.4998C36.5348 19.0783 29.9434 15.9565 27.3543 21.0086Z" fill="#1B1B1B"/>
+          </svg>
+
+
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegisterForm',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+
+.register-container {
+  background: hsl(var(--con));
+  border: 5px solid hsl(var(--txt));
+
+  font-size: calc(var(--_size));
+}
+
+
+.register-wrapper > *{
+  padding: 2rem;
+}
+
+.register-header{
+  background: hsl(var(--accent));
+
+  border-bottom: 5px solid hsl(var(--txt));
+}
+
+.register-input{
+  display: flex;
+  flex-direction: column;
+  gap: 2em;
+  padding-right: calc(var(--_size));
+  border-right: 5px solid hsl(var(--txt));
+}
+
+.register-content{
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+
+}
+
+.register-logo{
+  padding-left: calc(var(--_size));
+}
+
+form{
+  display: grid;
+  gap: .8em;
+
+  text-align: left;
+  padding-block: .5em;
+
+}
+
+
+
+.input-btn{
+
+}
+
+
+
+.input-txt:-webkit-autofill{
+  -webkit-text-fill-color: hsl(var(--accent-tertiary)) !important;
+  font-size: calc(var(--_size))!important;
+  font-family: 'JetBrains Mono', monospace !important;
+}
+
+.input-txt:-webkit-autofill::first-line{
+  -webkit-box-shadow: 0 0 0 100vmax white inset !important;
+  font-family: 'JetBrains Mono', monospace !important;
+  text-transform: uppercase;
+  font-size: calc(var(--_size)) !important;
+
+ }
+
+
+h3{
+  width: fit-content;
+}
+
+
+#triangle{
+  fill: hsl(var(--accent-secondary));
+}
+
+@media screen and (max-width: 700px) {
+
+
+  .register-content {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+    grid-auto-flow: row;
+  }
+
+  .register-logo * {
+    display: none;
+  }
+
+  .register-input {
+    padding-right: 0;
+    border-right: none;
+  }
+
+  @media screen and (max-width: 420px){
+    .input-group{
+      grid-template-columns: 1fr;
+      grid-template-rows: 1fr 1fr;
+
+    }
+  }
+}
+
+
+</style>
